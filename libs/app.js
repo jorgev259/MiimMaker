59ea46ec6be99c2f4a2f5deb4e29ee815d2eef64
rikumax25 <jorgev259@outlook.es>
2016-12-21 17:26:13 -0600

    Custom size of image and draggable test

--- libs/app.js
+++ libs/app.js
@@ -3,9 +3,9 @@ $( function() {
   } );
 
 function changeWidth(){
-    
+    $("#draggable").width($("#tw").val());
 }
 
 function changeHeight(){
-    
+    $("#draggable").height($("#th").val());
 }
