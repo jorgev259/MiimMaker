6d219ebbd7217f24bbd432970a112f5ea3aa8ffe
rikumax25 <jorgev259@outlook.es>
2016-12-21 20:18:00 -0600

    more debug

--- libs/app.js
+++ libs/app.js
@@ -54,5 +54,5 @@ function render(){
     window.open(canvas.toDataURL(),"toolbar=no");
   }
 });*/
- GrabzIt("ZjQ0MTllZWY0ZDRmNDg5ZTg2Y2JmNTEzZjgzZjJkOGQ=").ConvertHTML("<html><body><h1>Hello World!</h1></body></html>").DataURI();
+ GrabzIt("ZjQ0MTllZWY0ZDRmNDg5ZTg2Y2JmNTEzZjgzZjJkOGQ=").ConvertHTML("<html><body><h1>Hello World!</h1></body></html>").DataURI(callback);
 }
